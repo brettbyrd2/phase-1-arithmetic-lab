b97245898459f7b6f9c84d9b6200ba16545a90b0
Brett Byrd <byrdb2@yahoo.com>
2023-02-22 16:28:09 -0600

    complete

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+let num1 = 31
+let num2 = 2
+const multiply = (num1 * num2)
 
-// Write your code here
+
+
+
+const random = Math.floor(Math.random()* 10)+ 1
+
+let num3 = 36
+let num4 = 8
+const mod = (num3 % num4)
+
+
+const max = Math.max(1,5,7,16,20)
+
+
+
